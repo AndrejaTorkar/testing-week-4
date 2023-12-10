0da204db60daa9b131a106d4f3d4d3953466139c
AndrejaTorkar <104751136+AndrejaTorkar@users.noreply.github.com>
2023-12-10 13:57:58 +0100

    md-button-styles

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
+/* BUTTON STYLES */
+
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={[styles.blueSquare, {marginBottom:20}]}>
+        <Text style={styles.buttonText}>Button 1</Text>
+      </View>
+
+      <View style={[styles.blueSquare, styles.disabledSquare]}>
+        <Text style={styles.buttonText}>Button 2</Text>
+      </View>
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +26,38 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+blueSquare: {
+  backgroundColor: 'lightgreen',
+  borderColor: 'blue',
+  borderWidth: 5,
+  paddingTop: 10, 
+  paddingBottom: 10, // margine inside
+  paddingLeft: 20,
+  paddingRight: 20,
+  borderRadius: 20,
+  alignItems: 'center',
+  justifyContent: 'center',
+  // marginBottom: 20, // margin outside
+},
+
+disabledSquare: {
+  backgroundColor: 'lightgrey',
+  borderColor: 'lightgrey',
+  borderWidth: 5,
+  paddingTop: 10, 
+  paddingBottom: 10, // margine inside
+  paddingLeft: 20,
+  paddingRight: 20,
+  borderRadius: 20,
+  alignItems: 'center',
+  justifyContent: 'center',
+  // marginBottom: 20, // margin outside
+},
+
+buttonText: {
+  fontWeight: 'bold',
+  fontSize: 50,
+},
+
 });
